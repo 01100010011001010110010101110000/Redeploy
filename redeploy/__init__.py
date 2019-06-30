39b9ba01a20284c0c8cc83956ae85f3e12711c9e
Tyler Gregory <td.g@icloud.com>
2019-06-30 17:40:16 -0500

    Make annotation method more testable

--- redeploy/__init__.py
+++ redeploy/__init__.py
@@ -23,15 +23,20 @@ class Redeployer():
         self.namespace = namespace
         self.api_instance = kubernetes.client.AppsV1Api()
 
+    @staticmethod
+    def apply_restart_annotation(deployment, restart_time):
+        if deployment.spec.template.metadata.annotations is not None:
+            deployment.spec.template.metadata.annotations['restartedAt'] = restart_time
+        else:
+            deployment.spec.template.metadata.annotations = {
+                'restartedAt': restart_time
+            }
+        return deployment
+
     def redeploy(self):
         try:
             deployment = self.api_instance.read_namespaced_deployment(self.deployment, self.namespace)
-            if deployment.spec.template.metadata.annotations is not None:
-                deployment.spec.template.metadata.annotations['restartedAt'] = datetime.datetime.utcnow().isoformat()
-            else:
-                deployment.spec.template.metadata.annotations = {
-                    'restartedAt': datetime.datetime.utcnow().isoformat()
-                }
+            deployment = self.apply_restart_annotation(deployment, datetime.datetime.utcnow().isoformat())
             self.api_instance.patch_namespaced_deployment(self.deployment, self.namespace, deployment)
         except ApiException as e:
             logging.error(f"Error while redeploying {self.namespace}/{self.deployment}: {e.reason}")
